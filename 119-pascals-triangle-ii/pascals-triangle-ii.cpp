2c5e02f2b087dde7fe81ffc9ea53c21335e4a352
VikasKanwar101 <algowizard101@gmail.com>
2025-05-01 00:03:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (53.22%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        long long a=1;
-        vector<int> ans(rowIndex+1,1);
-        if(rowIndex==0) return ans;
-        for(int i=1;i<=rowIndex;i++)
+        long long n=1;
+        vector<int> a;
+        a.push_back(1);
+        for(int j=1;j<=rowIndex;j++)
         {
-            a=a*(rowIndex-i+1);
-            a=a/i;
-            ans[i]=a;
+            n=n*(rowIndex-j+1);
+            n=n/j;
+            a.push_back(n);
         }
-        return ans;
+        return a;
     }
 };
