ef37a2c1e5fe5c2e3be09cee10f8274a67f2f809
VikasKanwar101 <algowizard101@gmail.com>
2025-04-30 23:51:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (64.82%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> row(int i)
+    {
+        long long n=1;
+        vector<int> a;
+        a.push_back(1);
+        for(int j=1;j<=i;j++)
+        {
+            n=n*(i-j+1);
+            n=n/j;
+            a.push_back(n);
+        }
+        return a;
+    }
+    vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>>p;
+        if(numRows==1) return {{1}};
+        p.push_back({1});
+        p.push_back({1,1});
+        for(int i=2;i<numRows;i++)
+        {
+            p.push_back(row(i));
+        }
+        return p;
+    }
+};
