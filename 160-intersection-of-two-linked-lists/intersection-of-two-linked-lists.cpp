85e60c2f9941992ec8407984f838bf7a7193dc1b
VikasKanwar101 <algowizard101@gmail.com>
2025-05-17 15:42:18 +0530

    Time: 41 ms (37.02%) | Memory: 21.3 MB (13.57%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -6,21 +6,29 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+#include <unordered_set>
+
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode * A=headA;
-        while(A!=NULL)
-        {
-            ListNode* B=headB;
-            while(B!=NULL)
-            {
-                if(A==B)
-                return A;
-                B=B->next;
-            }
-            A=A->next;
+        std::unordered_set<ListNode*> nodeSet;
+
+        // Step 1: Store all nodes of list A in the hash set
+        ListNode* currA = headA;
+        while (currA != nullptr) {
+            nodeSet.insert(currA);
+            currA = currA->next;
         }
-        return nullptr;
+
+        // Step 2: Traverse list B and check for intersection
+        ListNode* currB = headB;
+        while (currB != nullptr) {
+            if (nodeSet.find(currB) != nodeSet.end()) {
+                return currB; // Intersection point found
+            }
+            currB = currB->next;
+        }
+
+        return nullptr; // No intersection found
     }
 };
