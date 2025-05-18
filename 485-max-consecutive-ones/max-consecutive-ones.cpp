0a85bbf7fb5aca03718223c8d40104d155a7da2a
VikasKanwar101 <algowizard101@gmail.com>
2025-05-18 16:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 50.1 MB (90.24%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -4,21 +4,13 @@ public:
         int c,m;
         c=m=0;
         int s=nums.size();
-        if(s==1)
-        {
-            if(nums[0]==1) return 1;
-            else return 0;
-        }
         for(int i=0;i<s;i++)
         {
-            if(nums[i]==1)
-            c++;
-            if(c>m)
-            m=c;
-            if(i==s-1)
-            break;
-            if(nums[i+1]==0)
-            c=0;
+            if(nums[i]==1){
+                c++;
+                m=max(c,m);
+            }
+            else c=0;
         }
         return m;
     }
