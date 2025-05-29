20cff8de7f319f1c5fc570a41c1dec940331dbf3
VikasKanwar101 <algowizard101@gmail.com>
2025-05-29 17:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (91.12%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void backtrack(vector<int>& candidates, int target, vector<int>& current, vector<vector<int>>& result, int index) {
+        if (target == 0) {
+            result.push_back(current);
+            return;
+        }
+        if (target < 0) return;
+        for (int i = index; i < candidates.size(); ++i) {
+            current.push_back(candidates[i]);
+            backtrack(candidates, target - candidates[i], current, result, i);  
+            current.pop_back();  
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> current;
+        backtrack(candidates, target, current, result, 0);
+        return result;
+    }
+};
