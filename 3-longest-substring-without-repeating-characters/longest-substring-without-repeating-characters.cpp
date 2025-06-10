addaa4e3081c7e64f55ddf6220a4f7d289a8e4ec
VikasKanwar101 <algowizard101@gmail.com>
2025-06-10 19:05:21 +0530

    Time: 3 ms (84.37%) | Memory: 11.3 MB (89.04%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        map<char,int>mpp;
-        int l,r,ml;
-        l=r=ml=0;
-        while(r<s.length()){
-            if(mpp.find(s[r])!=mpp.end())
-            l=max(l,mpp[s[r]]+1);  //make sure l doesn't move in the backward direction
-            mpp[s[r]]=r;   /*don't use else condition here because even if we find duplicate
-                            letter in the substring we have to still update it in 
-                            our substring and move the right pointer as well*/ 
-            ml=max(ml,r-l+1); 
-            r++;
+    int mpp[256];  // Tracks last index of each char
+    memset(mpp, -1, sizeof(mpp));  // Initialize all to -1
+    int l = 0, r = 0, ml = 0;
+
+    while (r < s.length()) {
+        if (mpp[s[r]] != -1) {
+            l = max(l, mpp[s[r]] + 1);  // Move 'l' past the last occurrence
         }
-        return ml;
+        mpp[s[r]] = r;  // Update last index
+        ml = max(ml, r - l + 1);
+        r++;
     }
+    return ml;
+}
 };
