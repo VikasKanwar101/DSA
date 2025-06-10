b38baab425a0d22c0f784e70b4f407ef1a75d690
VikasKanwar101 <algowizard101@gmail.com>
2025-06-10 18:54:44 +0530

    Time: 11 ms (51.86%) | Memory: 11.9 MB (48.28%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -6,8 +6,10 @@ public:
         l=r=ml=0;
         while(r<s.length()){
             if(mpp.find(s[r])!=mpp.end())
-            l=max(l,mpp[s[r]]+1);
-            mpp[s[r]]=r;
+            l=max(l,mpp[s[r]]+1);  //make sure l doesn't move in the backward direction
+            mpp[s[r]]=r;   /*don't use else condition here because even if we find duplicate
+                            letter in the substring we have to still update it in 
+                            our substring and move the right pointer as well*/ 
             ml=max(ml,r-l+1); 
             r++;
         }
