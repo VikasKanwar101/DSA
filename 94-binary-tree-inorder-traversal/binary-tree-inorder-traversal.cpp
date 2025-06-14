889f88bffbe1fd5d704a2fb464962cae820f65ef
VikasKanwar101 <algowizard101@gmail.com>
2025-06-14 11:49:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (6.61%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -13,8 +13,7 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        if(root==NULL)
-        return ans;
+        if(root==NULL) return ans;
         vector<int> left=inorderTraversal(root->left);
         ans.insert(ans.end(),left.begin(),left.end());
         ans.push_back(root->val);
