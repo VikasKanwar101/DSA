b58ef43a0ae2112620b4d368a5f91f7e0a4b4a1f
VikasKanwar101 <algowizard101@gmail.com>
2025-06-17 17:10:45 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (82.87%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     void rec(int ind,vector<vector<int>> &res,vector<int> &ds,vector<int>& candidates,int target){
-        if(ind==candidates.size()){
-            if(target==0){
-                sort(ds.begin(),ds.end());
-                res.push_back(ds);
-                sort(res.begin(),res.end());
-            }
+        if(target==0){
+            res.push_back(ds);
             return;
         }
-        if(candidates[ind]<=target){
-            ds.push_back(candidates[ind]);
-            rec(ind+1,res,ds,candidates,target-candidates[ind]);
+        for(int i=ind;i<candidates.size();i++){
+            if(candidates[i]>target) 
+            break;
+            if(i>ind && candidates[i]==candidates[i-1]) 
+            continue;
+            ds.push_back(candidates[i]);
+            rec(i+1,res,ds,candidates,target-candidates[i]);
             ds.pop_back();
         }
-        int nextind=ind+1;
-        while(nextind<candidates.size() && candidates[ind]==candidates[nextind])
-        nextind++;
-        rec(nextind,res,ds,candidates,target);
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
