fba4d9abd98c43c12e07b10c9db60aa6fb95c4c3
VikasKanwar101 <algowizard101@gmail.com>
2025-06-18 11:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (20.51%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    void recur(vector<int> &ds,vector<vector<int>> &ans,vector<int> &map,vector<int>& nums)
-    {
+    void rec(vector<int> &ds,vector<vector<int>> &ans,vector<int> &map,vector<int> nums){
         if(ds.size()==nums.size()){
             ans.push_back(ds);
             return;
         }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(map[i]==0){
+        for(int i=0;i<nums.size();i++){
+            if(!map[i]){
                 ds.push_back(nums[i]);
                 map[i]=1;
-                recur(ds,ans,map,nums);
+                rec(ds,ans,map,nums);
                 map[i]=0;
                 ds.pop_back();
             }
-            
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> ds;
         vector<int> map(nums.size(),0);
-        recur(ds,ans,map,nums);
+        rec(ds,ans,map,nums);
         return ans;
     }
 };
