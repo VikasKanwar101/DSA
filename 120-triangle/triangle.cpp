88adecb7476cb3ca74f315dddec747946eff5873
VikasKanwar101 <algowizard101@gmail.com>
2025-06-21 23:53:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (70.92%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
+        vector<int> front(n,0);
         for(int j=0;j<n;j++)
-        dp[n-1][j]=triangle[n-1][j];
+        front[j]=triangle[n-1][j];
         for(int i=n-2;i>=0;i--){
+            vector<int> cur(n,0);
             for(int j=i;j>=0;j--){
-                int d=triangle[i][j]+dp[i+1][j];
-                int dg=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(d,dg);
+                int d=triangle[i][j]+front[j];
+                int dg=triangle[i][j]+front[j+1];
+                cur[j]=min(d,dg);
             }
+            front=cur;
         }
-        return dp[0][0];
+        return front[0];
     }
 };
