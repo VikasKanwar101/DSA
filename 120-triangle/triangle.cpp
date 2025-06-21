f9d2867e10cd3c7c6ca571bce851889d29aa14e6
VikasKanwar101 <algowizard101@gmail.com>
2025-06-21 22:18:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (98.07%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,14 +2,9 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        // Start from the second last row and move upwards
-        for (int row = n - 2; row >= 0; row--) {
-            for (int col = 0; col <= row; col++) {
-                // For each element, add the minimum of the two adjacent numbers below it
-                triangle[row][col] += min(triangle[row + 1][col], triangle[row + 1][col + 1]);
-            }
-        }
-        // The top element now contains the minimum path sum
+        for (int row = n - 2; row >= 0; row--) 
+        for (int col = 0; col <= row; col++) 
+        triangle[row][col] += min(triangle[row + 1][col], triangle[row + 1][col + 1]);
         return triangle[0][0];
     }
 };
