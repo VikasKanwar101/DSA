0c5b5ffea923e569809b5e6512f18144e65e70eb
VikasKanwar101 <algowizard101@gmail.com>
2025-07-16 08:16:47 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (84.50%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    int f(int ind, vector<int>& nums, vector<int>& dp) {
-        if (ind >= nums.size() - 1) return 0;
-        if (dp[ind] != -1) return dp[ind];
-
-        int mini = INT_MAX;
-
-        for (int i = 1; i <= nums[ind]; i++) {
-            if (ind + i < nums.size()) {
-                int next = f(ind + i, nums, dp);
-                if (next != INT_MAX) {
-                    mini = min(mini, 1 + next);
-                }
-            }
-        }
-
-        return dp[ind] = mini;
-    }
-
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return f(0, nums, dp);
+        int j,l,r;
+        j=l=r=0;
+        while(r<nums.size()-1){
+            int f=0;
+            for(int i=l;i<=r;i++){
+                f=max(f,i+nums[i]);
+            }
+            j++;
+            l=r+1;
+            r=f;
+        }
+        return j;
     }
 };
