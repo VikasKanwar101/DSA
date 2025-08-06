de0ffe1ad1f9154c78ee73bad5ffe6ee518b43fe
VikasKanwar101 <algowizard101@gmail.com>
2025-08-06 12:09:07 +0530

    Time: 84 ms (29.43%) | Memory: 53.9 MB (7.99%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int subarraySum(vector<int>& a, int k) {
-        unordered_map<int,int> mpp;
+        map<int,int> mpp;
         mpp[0]=1;
-        int preSum=0;
-        int cnt=0;
+        int preSum,cnt;
+        preSum=cnt=0;
         for(int i=0;i<a.size();i++){
-            preSum+=a[i];
+            preSum=preSum+a[i];
             cnt+=mpp[preSum-k];
-            mpp[preSum]+=1;
+            mpp[preSum]++;
         }
         return cnt;
     }
