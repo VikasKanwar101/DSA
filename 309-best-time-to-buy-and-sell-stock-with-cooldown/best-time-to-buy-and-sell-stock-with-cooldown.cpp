b340304ad05e16587b8c6b7eb20e9d456f6564fe
VikasKanwar101 <algowizard101@gmail.com>
2025-08-15 15:08:00 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (22.83%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    int f(int ind, int buy, vector<int>& prices, int n, vector<vector<int>>& dp) {
-        if(ind >= n) return 0;
-
-        if(dp[ind][buy] != -1) return dp[ind][buy];
-
-        int profit = 0;
-        if(buy) {
-            profit = max(-prices[ind] + f(ind + 1, 0, prices, n, dp),
-                         0 + f(ind + 1, 1, prices, n, dp));
-        } else {
-            profit = max(prices[ind] + f(ind + 2, 1, prices, n, dp),
-                         0 + f(ind + 1, 0, prices, n, dp));
-        }
-
-        return dp[ind][buy] = profit;
+    int f(int ind,int buy,vector<int>& prices,int n,vector<vector<int>>& dp){
+        if(ind>=n) return 0;
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        if(buy) return dp[ind][buy]=max(-prices[ind]+f(ind+1,0,prices,n,dp),f(ind+1,1,prices,n,dp));
+        else return dp[ind][buy]=max(prices[ind]+f(ind+2,1,prices,n,dp),f(ind+1,0,prices,n,dp));
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return f(0, 1, prices, n, dp);
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return f(0,1,prices,n,dp);
     }
 };
