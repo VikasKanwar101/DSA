557b63ed5ca82502a26dbb4903302a1482c9721e
VikasKanwar101 <algowizard101@gmail.com>
2025-08-15 15:11:07 +0530

    Time: 124 ms (6.27%) | Memory: 100.3 MB (29.25%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int f(int ind, int buy, vector<int>& prices, int n, vector<vector<int>>& dp,int fee) {
-        if(ind == n) return 0;
-
-        if(dp[ind][buy] != -1) return dp[ind][buy];
-
-        int profit = 0;
-        if(buy) {
-            profit = max(-prices[ind] + f(ind + 1, 0, prices, n, dp,fee),
-                         0 + f(ind + 1, 1, prices, n, dp,fee));
-        } else {
-            profit = max(prices[ind] - fee + f(ind + 1, 1, prices, n, dp,fee),
-                         0 + f(ind + 1, 0, prices, n, dp,fee));
+    int f(int ind,int buy,vector<int>& prices,int &n,vector<vector<int>>& dp,int fee){
+        if(ind==n) return 0;
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        int take=1e9;
+        int nottake=1e9;
+        if(buy==1){
+            take=-prices[ind]+f(ind+1,0,prices,n,dp,fee);
+            nottake=f(ind+1,1,prices,n,dp,fee);
         }
-
-        return dp[ind][buy] = profit;
+        else{
+            take=prices[ind]-fee+f(ind+1,1,prices,n,dp,fee);
+            nottake=f(ind+1,0,prices,n,dp,fee);
+        }
+        return dp[ind][buy]=max(take,nottake);
     }
-    int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return f(0, 1, prices, n, dp , fee);
+    int maxProfit(vector<int>& prices,int fee) {
+        int n=prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return f(0,1,prices,n,dp,fee);
     }
 };
