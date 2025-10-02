25dbc75eca6046991f37357bc7b426e841ccf5b6
VikasKanwar101 <algowizard101@gmail.com>
2025-10-02 08:42:49 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (99.61%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -4,8 +4,7 @@ public:
         int l=nums.size();
         int s=(l*(l+1))/2;
         int sum=0;
-        for(int i=0;i<l;i++)
-        {
+        for(int i=0;i<l;i++){
             sum=sum+nums[i];
         }
         return s-sum;
